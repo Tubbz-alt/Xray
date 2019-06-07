810d4c72d4f776e977c3fb66b8f9940ea25815b3
josef frisch <frisch@slac.stanford.edu>
2019-06-07 16:35:14 -0700

    added code for testing sdf

--- pulsedetect.cpp
+++ pulsedetect.cpp
@@ -1,12 +1,23 @@
 // pulsedetect.cpp
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
 #include pulsedetect.h
+
 int main(int argc, char *argv[])
 {
   uint8_t *buffer;
 
   buffer = (uint8_t*) malloc(BUFFER_S * sizeof(uint8_t));
   
+
 
 
 }
