fb1101806be1742ebd3d406874963e4a782ab137
josef frisch <frisch@slac.stanford.edu>
2019-06-07 16:36:55 -0700

    dding code

--- pulsedetect.h
+++ pulsedetect.h
@@ -1,4 +1,7 @@
 // pulsedetect.h
 BUFFER_S = 100000000; // buffer in bytes
-HEADER_S = 1000; // bytes in header
-DATA_S = 8000;
+HEADER_S = 1024; // bytes in header
+
+CHAN_COUNT_OFFSET = 4; // offset in header to channel count
+CHAN_COUNT_WIDTH = 4;  // bytes
+DATA_WIDTH = 2;  / 2 byte data
