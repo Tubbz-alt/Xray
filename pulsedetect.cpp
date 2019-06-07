6043759ea3b62895aefae9076e306977807446c5
josef frisch <frisch@slac.stanford.edu>
2019-06-07 15:37:46 -0700

    Initial program add

---
+++ pulsedetect.cpp
@@ -0,0 +1,12 @@
+// pulsedetect.cpp
+
+#include pulsedetect.h
+int main(int argc, char *argv[])
+{
+  uint8_t *buffer;
+
+  buffer = (uint8_t*) malloc(BUFFER_S * sizeof(uint8_t));
+  
+
+
+}
---
+++ pulsedetect.h
@@ -0,0 +1,4 @@
+// pulsedetect.h
+BUFFER_S = 100000000; // buffer in bytes
+HEADER_S = 1000; // bytes in header
+DATA_S = 8000;
